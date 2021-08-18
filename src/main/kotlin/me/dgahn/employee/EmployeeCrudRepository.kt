1d2d7dca7125ba91e366692f2441de700026cc98
dgahn <dgahn@si-analytics.ai>
2021-08-18 23:44:26 +0900

    feat: r2dbc flow 예제 추가

--- src/main/kotlin/me/dgahn/employee/EmployeeCrudRepository.kt
+++ src/main/kotlin/me/dgahn/employee/EmployeeCrudRepository.kt
@@ -1,7 +1,14 @@
 package me.dgahn.employee
 
+import kotlinx.coroutines.flow.Flow
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface EmployeeCrudRepository : CoroutineCrudRepository<Employee, String>
+interface EmployeeCrudRepository : CoroutineCrudRepository<Employee, String> {
+
+    suspend fun findByName(name: String): Employee
+
+    fun findAllByNameStartsWith(name: String): Flow<Employee>
+
+}
--- src/test/kotlin/me/dgahn/employee/EmployeeRepositoryTest.kt
+++ src/test/kotlin/me/dgahn/employee/EmployeeRepositoryTest.kt
@@ -2,8 +2,12 @@ package me.dgahn.employee
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectIndexed
+import kotlinx.coroutines.flow.toList
 import me.dgahn.R2bcConfiguration
 import org.springframework.test.context.ContextConfiguration
 
@@ -38,6 +42,41 @@ class EmployeeRepositoryTest(employeeRepo: EmployeeCrudRepository) : FunSpec({
 
         find2!!.name shouldBe newName
         find1 shouldNotBe find2
+    }
+
+    test("직원을 이름으로 조회할 수 있다.") {
+        val employee = Employee(name = "test")
+        val saved = employeeRepo.save(employee)
+
+        val find = employeeRepo.findByName(employee.name)
+        find shouldBe saved
+    }
+
+    test("직원 목록을 조회할 수 있다.") {
+        employeeRepo.saveAll((1..1000).map { Employee(name = "test_$it") }).collect()
+
+        val find = employeeRepo.findAll()
+        find.collectIndexed { index, value ->
+            println(value.name)
+            value.name shouldBe "test_${index + 1}"
+        }
+    }
+
+    test("test로 시작하는 직원 목록을 조회할 수 있다.") {
+        val name = "test"
+        employeeRepo.saveAll((1..100).map { Employee(name = "test_$it") }).collect()
+
+        val find = employeeRepo.findAllByNameStartsWith(name)
+        find.toList() shouldHaveSize 100
+        find.collect { employee -> employee.name.startsWith("test") }
+    }
+
+    test("test로 저장하면서 다른 일을 할 수 있다.") {
+        employeeRepo.saveAll((1..100).map { Employee(name = "test_$it") })
+            .collect { println(it.name) }
+
+        val find = employeeRepo.findAll()
+        find.toList() shouldHaveSize 100
     }
 
 })
